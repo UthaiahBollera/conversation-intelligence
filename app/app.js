0f0e019b17123e633949122772f27385cea58f45
Uthaiah BB <U.Bollerabopaiah@walmartlabs.com>
2020-03-08 00:16:03 +0530

    added share component

--- app/app.js
+++ app/app.js
@@ -5,12 +5,14 @@ import event from "./event.js";
 import Transcript from "../app/components/transcript/index.js";
 import SearchTranscript from "./components/search-transcript/index.js";
 import SoundBar from "./components/sound-bar/index.js";
+import Share from "./components/share/index.js";
 
 window.event = event;
 window.addEventListener('DOMContentLoaded', (event) => {
   document.querySelector('#player').prepend(CovaiAudioPlayer.render());
-  document.querySelector('#convai-player').insertAdjacentHTML("afterend", PlaySpeedAdjust.render());
+  document.querySelector('#convai-player').insertAdjacentElement("afterend", PlaySpeedAdjust.render());
   document.querySelector('#sound-bar').appendChild(SoundBar.render());
   document.querySelector('#search-transcript').insertAdjacentElement("afterend", Transcript.render());
   document.querySelector('#search-transcript').appendChild(SearchTranscript.render());
+  document.querySelector('#player').appendChild(Share.render());
 });
